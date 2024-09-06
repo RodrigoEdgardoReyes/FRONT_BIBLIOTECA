352fe76a8c596a8e3767d7dda3c2d791bd0e7640
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-09-05 23:28:59 -0600

    main-ts :tada::sparkles:

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue'
 import router from './router';
 import './assets/tailwind.css';
 //import router from './router'
+//import './assets/css/app.css'
 
 createApp(App)
   .use(router)
