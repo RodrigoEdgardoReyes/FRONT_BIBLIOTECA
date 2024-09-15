ec13865b52b5507279f073e74f8f0233db83d2f2
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-09-14 20:26:56 -0600

    vista libros :tada::sparkles:

--- src/views/Libros.vue
+++ src/views/Libros.vue
@@ -1,14 +1,15 @@
-<template>
+<!-- <template>
   <div class="container mx-auto p-8">
     <h1 class="text-3xl font-bold text-gray-400 mb-6">Gestión de Libros</h1>
     <LibroForm />
     <LibroList />
   </div>
-</template>
+</template> -->
 
 <script>
-import LibroForm from '../components/LibroForm.vue'
-import LibroList from '../components/LibroList.vue'
+import LibroForm from '../components/libroForm.vue'
+// import LibroForm from '../components/LibroForm.vue'
+import LibroList from '../components/libroList.vue'
 
 export default {
   components: {
