3700ecb23801aee0fe532fc8987d4436e968e8e8
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-09-14 20:28:36 -0600

    configuracion de rutas :tada::sparkles:

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,53 @@
 // @ts-nocheck
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import Libros from '../views/Libros.vue'
+import Libros from '../components/LibroForm.vue'
+import Lista from '../components/LibroForm.vue'
 import Prestamos from '../views/Prestamos.vue'
 import ListLibros from '../views/Lista.vue'
+import dashboard from '../components/master/dashboard.vue'
+import home from '../components/home.vue'
+import profile from '../components/profile.vue'
 
 const routes = [
-  { path: '/', name: 'Home', component: Home },
+  // { path: '/', name: 'Home', component: Home },
   { path: '/libros', name: 'Libros', component: Libros },
-  { path: '/prestamos', name: 'Prestamos', component: Prestamos }
+  { path: '/prestamos', name: 'Prestamos', component: Prestamos },
   // { path: '/lista', name: 'Lista', component: Lista }
-]
+  {
+    name: 'Dashboard',
+    path: '/',
+    component: dashboard,
+    children: [
+
+  {
+    name: 'home',
+    path: '/home',
+    component: home
+  },
+  {
+    name: 'profile',
+    path: '/profile',
+    component: profile
+  },
+  {
+    path: 'libros',
+    name: 'Libros',
+    component: Libros
+  },
+  {
+    path: 'prestamos',
+    name: 'Prestamos',
+    component: Prestamos
+  },
+  {
+    path: 'lista',
+    name: 'Lista',
+    component:Libros
+  }
+  ]
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(),
