3c6026379b837e4e17a41ede0da698797aa86b75
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-09-15 22:10:15 -0600

    config vite :tada::sparkles:

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // server: {
-  //   proxy: {
-  //     '/libros': {
-  //       target: 'http://localhost:3000',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/libros/, '')
-  //     }
-  //   }
-  // }
 
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3000',  
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
