dea54191a894431ec8d2f7ad579cd288591c2d90
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-09-18 20:11:00 -0600

    config js

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 // si deseo usar esta url en otro archivo lo importo de la siguiente manera 
 // import { API_BASE_URL } from '../config';
 
-export const API_BASE_URL = 'http://localhost:3000'; // Cambia esto por la URL de tu API
+export const API_BASE_URL = 'http://localhost:3000'; // Cambia esto por la URL de tu API.
