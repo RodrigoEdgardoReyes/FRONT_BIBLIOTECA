cfcc2f7450f7e8fbd3dad66a54def4081809ebe0
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-15 22:11:09 -0600

    vista de Listado de libros :tada::sparkles:

---
+++ src/views/LibroList.vue
@@ -0,0 +1,14 @@
+<template>
+    <!-- <div class="text-3xl text-black">Listado de libros existentes</div> -->
+    <LibroList/>
+</template>
+
+<script>
+import LibroList from '../components/LibroList.vue';
+
+    export default {
+        components: {
+            LibroList
+        }
+    }
+</script>
