d80f3b5f1d0a558811f9d9ce384a48d8236ae44b
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-19 23:57:06 -0600

    home :tada::sparkles:

--- src/components/home.vue
+++ src/components/home.vue
@@ -11,7 +11,7 @@ const goToPrestar = () => {
   router.push({ name: 'Prestamos' });
 };
 </script>
-<!-- home.. FALTA MEJORARLO -->
+<!-- home.. FALTA MEJORARLO -->.
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen bg-gray-600">
     <h1 class="text-4xl font-bold text-gray-800 mb-6">Bienvenido a la Biblioteca.</h1>
