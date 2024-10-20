b2b98160e6d6ebb1b66a3ea7008f0c7029d384eb
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-19 23:58:22 -0600

    libros :tada::sparkles:

--- src/views/Libros.vue
+++ src/views/Libros.vue
@@ -1,10 +1,27 @@
-<template>
+- <template>
   <div class="container mx-auto p-8">
     <h1 class="text-3xl font-bold text-gray-400 mb-6">Gestión de Libros</h1>
     <LibroForm />
     <!-- <LibroList /> -->
   </div>
 </template>
+
+<!-- <template>
+  <div class="h-screen flex flex-col">
+    <header class="bg-gray-800 text-white p-4">
+      <div class="container mx-auto p-8">
+    <h1 class="text-3xl font-bold text-gray-400 mb-6">Gestión de Libros</h1>
+    <LibroForm />
+    <LibroList />
+  </div>
+    </header>
+    <div class="flex-grow overflow-auto bg-gray-100">
+      <router-view></router-view>
+    </div>
+  </div>
+</template> -->
+
+
 
 <script>
 import LibroForm from '../components/libroForm.vue'
@@ -14,7 +31,7 @@ import LibroList from '../components/libroList.vue'
 export default {
   components: {
     LibroForm,
-    // LibroList
+    LibroList
   }
 }
 </script>
