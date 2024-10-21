391306c02297c6d7f2d1a3409d78072f94414537
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-20 18:32:22 -0600

    Listado de libros completado :tada::sparkles:

--- src/views/LibroList.vue
+++ src/views/LibroList.vue
@@ -1,15 +1,35 @@
-<template>
-    <!-- FALTA -->
-    <!-- <div class="text-3xl text-black">Listado de libros existentes</div> -->
+<!-- <template>
+    <div class="text-3xl text-black">Listado de libros existentes</div> 
     <LibroList/>
+</template> -->
+
+<template>
+    <div class="h-screen flex flex-col content">
+        <!-- <header class="bg-gray-800 text-white p-4">
+      <div></div>
+    </header> -->
+        <div class="flex-grow overflow-auto bg-gray-100">
+            <router-view>
+                <LibroList />
+            </router-view>
+        </div>
+    </div>
 </template>
 
 <script>
 import LibroList from '../components/LibroList.vue';
 
-    export default {
-        components: {
-            LibroList
-        }
+export default {
+    components: {
+        LibroList
     }
+}
 </script>
+
+<style scoped>
+.content {
+    display: flex;
+    max-height: 80vh;
+    width: auto;
+}
+</style>
