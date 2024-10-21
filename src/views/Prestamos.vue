51c48f0a1cc030a3fe6a02c76d539da8d27569c2
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-20 18:33:48 -0600

    formulario de prestamos :tada::sparkles:

--- src/views/Prestamos.vue
+++ src/views/Prestamos.vue
@@ -1,20 +1,32 @@
 <template>
-  <div class="container mx-auto p-8">
-    <h1 class="text-2xl font-bold text-center mb-6">Préstamoss.</h1>
-    <PrestamoForm />
-    <!-- <PrestamoList /> -->
+  <div class="h-screen flex flex-col content">
+
+    <h1 class="text-2xl font-bold text-center mb-3 text-gray-300">¿Quieres prestar un libro?</h1>
+    <div class="flex-grow overflow-auto bg-gray-100">
+      <router-view>
+        <PrestamoForm />
+        <!-- <PrestamoList /> -->
+      </router-view>
+    </div>
+
   </div>
 </template>
 
 <script>
 // rutas de los componentes
 import PrestamoForm from '../components/PrestamoForm.vue'
-import PrestamoList from '../components/PrestamoList.vue'
+// import PrestamoList from '../components/PrestamoList.vue'
 
 export default {
   components: {
     PrestamoForm,
-    // PrestamoList.
+    // PrestamoList
   }
 }
 </script>
+
+<style scoped>
+.content {
+  max-height: 80vh;
+}
+</style>
