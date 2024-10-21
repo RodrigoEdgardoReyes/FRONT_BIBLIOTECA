66140a754677e4951a73691bf67f9548be1fc099
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-20 18:34:14 -0600

    listado de prestamos :tada::sparkles:

---
+++ src/views/PrestamoList.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <!-- Si descomento esto el texto queda estatico  -->
+    <!-- <div class="bg-black content">Listado de prestamos</div> -->
+
+    <div  class="h-screen flex flex-col content">
+
+        <div class="flex-grow overflow-auto bg-gray-100">
+        <router-view>
+            <PrestamoList />
+        </router-view>
+
+    </div>
+    </div>
+</template>
+
+<script>
+import PrestamoList from '../components/PrestamoList.vue';
+
+export default {
+    components: {
+        PrestamoList
+    }
+}
+</script>
+
+<style scoped>
+.content {
+    background-color: red;
+    max-height: 80vh;
+}
+</style>
