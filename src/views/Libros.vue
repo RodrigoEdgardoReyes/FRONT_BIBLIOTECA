782c5cad3864c46e2d1f33608b2e211a4f1e082a
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-26 20:21:13 -0600

    Libros :tada::sparkles:

--- src/views/Libros.vue
+++ src/views/Libros.vue
@@ -1,6 +1,6 @@
 - <template>
   <div class="container mx-auto p-8">
-    <h1 class="text-3xl font-bold text-gray-400 mb-6">Gestión de Libros</h1>
+    <h1 class="text-3xl font-bold text-gray-400 mb-6">Registro de Libros</h1>
     <LibroForm />
     <!-- <LibroList /> -->
   </div>
